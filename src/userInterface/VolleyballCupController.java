76470f2410af7d30dfc4b5c67a4ab3768543f22e
Carlos Restrepo <cr461204@gmail.com>
2019-05-15 19:36:29 -0500

    creation of interface with Scene Builder

---
+++ src/userInterface/VolleyballCupController.java
@@ -0,0 +1,71 @@
+package userInterface;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.shape.Rectangle;
+
+public class VolleyballCupController {
+
+    @FXML
+    private Label timeSearchParticipant;
+
+    @FXML
+    private Label informationLabel;
+
+    @FXML
+    private ScrollPane fieldToPaint;
+
+    @FXML
+    private Label timeSearchSpectator;
+
+    @FXML
+    private Rectangle avatarImage;
+
+    @FXML
+    void exploreFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fieldFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void idSpectator(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchSpectator(ActionEvent event) {
+
+    }
+
+    @FXML
+    void idParticipant(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchParticipant(ActionEvent event) {
+
+    }
+
+    @FXML
+    void paintParticipantStructure(ActionEvent event) {
+
+    }
+
+    @FXML
+    void paintSpectatorStructure(ActionEvent event) {
+
+    }
+
+}
